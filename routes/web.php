66b76b9bd248248bd702fc3435efe2d957f42e51
Syaddad Raihan Putra <putrasyaddad@gmail.com>
2026-05-15 20:43:36 +0700

    Add Home Controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'loginSubmit'])->name('login.submit');
