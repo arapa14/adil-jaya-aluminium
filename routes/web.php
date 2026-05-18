5fb36a08cb83407b8fc1f13117c531a5d7665ffe
arapa14 <arapa00016@gmail.com>
2026-05-18 21:41:32 +0700

    add: CRUD portfolio category page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SEOController;
 use App\Http\Controllers\SettingController;
 use App\Http\Middleware\AdminMiddleware;
-use App\Models\Portfolio;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'home'])->name('home');
