4eee5321fc79d6dd12383658aafb1a0d45df7b35
arapa14 <arapa00016@gmail.com>
2026-05-19 20:20:26 +0700

    add: service menu && fix: appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
                 'hero_image',
                 'whatsapp',
                 'address',
+                'email',
             ])
         ) {
             View::share([
