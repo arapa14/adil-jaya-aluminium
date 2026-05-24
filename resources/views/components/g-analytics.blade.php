9c4294497fe8e754f8310c8b3473464a18ca6887
Syaddad Raihan Putra <putrasyaddad@gmail.com>
2026-05-24 19:07:30 +0700

    Change hotjar to google analytics

--- resources/views/components/g-analytics.blade.php
+++ resources/views/components/g-analytics.blade.php
@@ -1,1 +1,9 @@
-<script src="https://t.contentsquare.net/uxa/0c46afd018c49.js"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V9P40PHYCE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V9P40PHYCE');
+</script>
